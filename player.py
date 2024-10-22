b3641dea1cb186826a158e2d9894063bcc953fd0
augustine-a8 <logos.agbeko@gmail.com>
2024-10-22 17:39:36 +0100

    game play for single player

--- player.py
+++ player.py
@@ -12,7 +12,20 @@ class Player:
     
     def show_hand(self):
         all_cards_str = ""
-        for card in self.hand:
-            all_cards_str += f"{card[1]} of {card[0]}\t"
+        for i in range(0, len(self.hand)):
+            card = self.hand[i]
+            all_cards_str += f"{i+1}. {card[1]} of {card[0]}\t"
         print(f"{self.name}'s Deck")
         print(all_cards_str)
+    
+    def play_card(self):
+        self.show_hand()
+        card_index = int(input("Pick card: "))
+        if card_index > len(self.hand) or card_index < 1:
+            raise IndexError("Card index out of bounds")
+        played_card = self.hand.pop(card_index - 1)
+        print(f"{self.name} played {played_card[1]} of {played_card[0]}")
+        return played_card
+
+    def hand_is_empty(self):
+        return len(self.hand) == 0
--- spar.py
+++ spar.py
@@ -6,6 +6,7 @@ class Spar:
     def __init__(self):
         self.deck_of_cards = []
         self.players = []
+        self.trick_winning_card = ()
 
         self.shuffle_deck()
 
@@ -15,20 +16,23 @@ class Spar:
             for rank in RANKS:
                 self.deck_of_cards.append((suit, rank))
         random.shuffle(self.deck_of_cards)
+
+    def __get_random_card_from_deck(self):
+        i = random.randint(0, len(self.deck_of_cards) - 1)
+        return self.deck_of_cards.pop(i) 
     
     def deal_hands_to_player(self, player):
         first_set = 3
         while first_set > 0:
-            i = random.randint(0, len(self.deck_of_cards))
-            player.add_card_to_hand(self.deck_of_cards.pop(i))
+            player.add_card_to_hand(self.__get_random_card_from_deck())
             first_set -= 1
         second_set = 2
         while second_set > 0:
-            i = random.randint(0, len(self.deck_of_cards))
-            player.add_card_to_hand(self.deck_of_cards.pop(i))
+            player.add_card_to_hand(self.__get_random_card_from_deck())
             second_set -= 1
 
     def __single_player(self):
+        self.players = []
         print("SPAR SINGLE PLAYER MODE")
         name = input("Enter player name: ")
         player = Player(name)
@@ -39,8 +43,28 @@ class Spar:
         for player in self.players:
             self.deal_hands_to_player(player)
         
-        for player in self.players:
-            player.show_hand()
+        random.shuffle(self.players)
+        player_one = self.players[0]
+        player_two = self.players[1]
+
+        while not player_one.hand_is_empty() and not player_two.hand_is_empty():
+            print(f"{player_one.name}'s turn")
+            p1_card = player_one.play_card()
+            print(f"{player_two.name}'s turn")
+            p2_card = player_two.play_card()
+            trick_winner = self.__compare_cards(p1_card, p2_card)
+            if trick_winner == 1:
+                print(f"{player_one.name} won turn with {p1_card[1]} of {p1_card[0]}")
+            else:
+                print(f"{player_two.name} won turn with {p2_card[1]} of {p2_card[0]}")
+
+    def __compare_cards(self, card_one, card_two):
+        if card_one[0] == card_two[0]:
+            if RANKS.index(card_one[1]) >= RANKS.index(card_two[1]):
+                return 1
+            else:
+                return 2
+        return 1
 
     def __multiplayer(self):
         pass
